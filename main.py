12db19c229cc3f2033925e3da14b400826f86252
Zafir <zafir.nasim@gmail.com>
2026-02-07 23:53:03 -0600

    add challenges, checking, front end

--- main.py
+++ main.py
@@ -33,5 +33,42 @@ def check_on_dfa(request: DFARequest):
             "success": False,
             "errors": [f"Server error: {str(e)}"]
         }
+    
+class DFASubmitRequest(BaseModel):
+    yaml_def: str
+    challenge: str
+    solution: str
+
+@app.post("/submit")
+def submit_dfa_solution(request: DFASubmitRequest):
+    try:
+        dfa = DFA()
+        load_result = dfa.load(request.yaml_def, is_file=False)
+        if not load_result["success"]:
+            return load_result
+
+        solution = DFA()
+        solution_load_result = solution.load("solutions/"+request.solution)
+        if not solution_load_result["success"]:
+            return {
+                "success": False,
+                "errors": [f"Error loading solution: {solution_load_result['errors']}"]
+            }
+        
+        compare = dfa ^ solution
+        example = compare.get_example()
+
+        return {
+            "success": example is None,
+            "example": example
+        }
+
+    
+    except Exception as e:
+        return {
+            "success": False,
+            "errors": [f"Server error: {str(e)}"]
+        }
+
 
 app.mount("/", StaticFiles(directory=".", html=True), name="static")
